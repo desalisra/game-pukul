13d8f4a645d464f8596e2e3499467f021b072c0b
desalisra <desalisra48@gmail.com>
2021-04-10 20:54:15 +0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const tanah = document.querySelectorAll('.tanah');
+const tikus = document.querySelectorAll('.tikus');
+const papanSkor = document.querySelector('.papan-skor');
+const pop = document.querySelector('#pop');
+const dp = document.querySelector('.row');
+
+let tanahSebelumnya;
+let selesai;
+let skor;
+
+function randomTanah(tanah) {
+  const t = Math.floor(Math.random() * tanah.length);
+  const tRandom = tanah[t];
+  if (tRandom == tanahSebelumnya) {
+    randomTanah(tanah);
+  }
+  tanahSebelumnya = tRandom;
+  return tRandom;
+}
+
+function randomWaktu(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function munculkanTikus() {
+  const tRandom = randomTanah(tanah);
+  const wRandom = randomWaktu(300, 1000);
+  tRandom.classList.add('muncul');
+
+  setTimeout(() => {
+    tRandom.classList.remove('muncul');
+    if (!selesai) {
+      munculkanTikus();
+    }
+  }, wRandom);
+}
+
+function mulai() {
+  dp.classList.remove('d-none')
+
+  selesai = false;
+  skor = 0;
+  papanSkor.textContent = 0;
+  munculkanTikus();
+  setTimeout(() => {
+    selesai = true;
+    dp.classList.add('d-none')
+  }, 10000);
+}
+
+function pukul() {
+  skor++;
+  this.parentNode.classList.remove('muncul');
+  pop.play();
+  papanSkor.textContent = skor;
+}
+
+tikus.forEach(t => {
+  t.addEventListener('click', pukul);
+});
